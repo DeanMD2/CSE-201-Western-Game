ffda589f14d0a2e5a2837b732571457eb3a9f0e9
Dean <dicarldm@miamioh.edu>
2025-04-02 14:38:03 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player extends ThePlayer1 {
+    
+	private List<String> pokerHand;
+	private String name;
+	private int money;
+	private int score;
+    
+    public Player(String name, int money, int score) {
+    	super(name, money, score);
+    	this.name = name;
+    	this.money = money;
+    	this.score = score;
+		this.pokerHand = new ArrayList<>();
+	}
+
+	//poker methods below:
+    public void receiveCard(String card) {
+        pokerHand.add(card);
+    }
+    
+    public void showHand() {
+        System.out.println(name + "'s hand: " + pokerHand);
+    }
+    
+    public List<String> getHand() {
+        return pokerHand;
+    }
+    
+    public void clearHand() {
+    	pokerHand.clear();
+    }
+}
---
+++ ThePlayer1.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThePlayer1 {
+
+	
+	private String name;
+    private int health;
+    private int money;
+    private int score;
+    
+    
+    public ThePlayer1(String name, int money, int score) {
+    	this.name = name;
+        health = 10;
+        this.money = money;
+        this.score = score;
+    }
+    
+    public void move() {
+    	
+    }
+    
+    public void setScore(int score) {
+    	this.score = score;
+    }
+    
+    public int getScore() {
+    	return score;
+    }
+    
+    public void setHealth(int health) {
+    	this.health = health;
+    }
+    
+    public int getHealth() {
+    	return health;
+    }
+    
+    public void setMoney(int money) {
+    	this.money = money;
+    }
+    
+    public int getMoney() {
+    	return money;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+}
