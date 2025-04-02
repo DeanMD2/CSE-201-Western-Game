96a500ea80013f82ccc4107f65f547689bad00da
xinghanwen2 <xingh2@miamioh.edu>
2025-04-02 14:44:50 -0400

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -2,9 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Shop {
+    
+double price;
 
     puclic dispayMessage() {
         System.out.print("Shop is under construction");
+    }
+
+    public int getPrice() {
+        return price;
     }
 
 }
