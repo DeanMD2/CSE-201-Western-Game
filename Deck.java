5fae7fe9fa25043ee10144b2597ebe57bc623806
Dean <dicarldm@miamioh.edu>
2025-04-04 14:25:21 -0400

    Add files via upload

--- Deck.java
+++ Deck.java
@@ -1,64 +1,49 @@
-import java.util.Collections;
 import java.util.Random;
 
 public class Deck {
-	
-	private static int DECK_SIZE = 52;
-	private String[] theDeck;
-	private int currCardIndex;
-	
-	public Deck() {
-		
-		String[] cards = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-		String[] suits = { "♥", "♠", "♣", "♦"};
-		theDeck = new String[DECK_SIZE];
-		currCardIndex = 0;
-		
-		for (int i = 0; i < suits.length; i++) {
-			for (int j = 0; j < cards.length; j++) {
-				int index = cards.length * i + j;
-				theDeck[index] = (cards[j] + suits[i]);
-			}
-		}
-	}
-	
-	public void Shuffle() {
-		Random rand = new Random();
-		for (int i = theDeck.length - 1; i > 0; i--) {
-			int j = rand.nextInt(i + 1);
-			String temp =  theDeck[i];
-			theDeck[i] = theDeck[j];
-			theDeck[j] = temp;
-			
-		}
-		
-		currCardIndex = 0;
-		
-	}
-	
-	public String dealCard() {
-		if (currCardIndex < DECK_SIZE) {
-			return theDeck[currCardIndex++];
-		} else {
-			return null;
-		}
-	}
-	
-	public void Print() {
-		for (int i = 0; i < theDeck.length; i++) {	
-			System.out.print(theDeck[i] + " ");
-			if ((i + 1) % 13 == 0) {
-				System.out.println();
-			}
-		}
-	}
-		
-	
-	public String[] getDeck() {
-		return theDeck;
-	}
-	
-	
-		
-	
+
+    private static final int DECK_SIZE = 52;
+    private Card[] theDeck;
+    private int currCardIndex;
+
+    public Deck() {
+        Card.Suit[] suits = Card.Suit.values();
+        Card.Rank[] ranks = Card.Rank.values();
+        theDeck = new Card[DECK_SIZE];
+        int index = 0;
+        for (Card.Suit suit : suits) {
+            for (Card.Rank rank : ranks) {
+                theDeck[index++] = new Card(rank, suit);
+            }
+        }
+        currCardIndex = 0;
+    }
+
+    public void shuffle() {
+        Random rand = new Random();
+        for (int i = theDeck.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            Card temp = theDeck[i];
+            theDeck[i] = theDeck[j];
+            theDeck[j] = temp;
+        }
+        currCardIndex = 0;
+    }
+
+    public Card dealCard() {
+        if (currCardIndex < DECK_SIZE) {
+            return theDeck[currCardIndex++];
+        } else {
+            return null;
+        }
+    }
+
+    public void printDeck() {
+        for (int i = 0; i < theDeck.length; i++) {
+            System.out.print(theDeck[i] + " ");
+            if ((i + 1) % 13 == 0) {
+                System.out.println();
+            }
+        }
+    }
 }
